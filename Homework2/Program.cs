1b328ec20e1bbaf1b7e3c2a154c4d7e60a1903e1
LandyshH <86878681+LandyshH@users.noreply.github.com>
2021-09-26 12:23:40 +0300

    2k 248 (#4)
    * 1hw
    
    * 1hw
    
    * tests added
    
    * tests added
    
    * tests added
    
    * tests
    
    * tests
    
    * tests added
    
    * created README
    
    * move sln
    
    * Create dotNet.yml
    
    * refactoring
    
    * refactoring
    
    * Refactoring
    
    * refactoring
    
    * refactoring
    
    * Tests refactoring
    
    * tests refactoring
    
    * homework2 and homework2IL added
    
    * Calculator il
    
    * il refactoring
    
    * il refactoring
    
    * parser and calculator il
    
    * global json
    
    * global json added
    
    * gitignore idea
    
    * idea folder deleted from git
    
    * Revert "idea folder deleted from git"
    
    This reverts commit 9cb9c20d8b1d59ce429132957afb1125e0cc66f7.
    
    * deleted idea

---
+++ Homework2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Homework2IL;
+
+
+namespace Homework2
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var resultIs = Parser.ParseArguments(args, out var problem);
+            if (resultIs != HundledExceptions.Success) return (int)resultIs;
+            var result = Calculator.Calculate(problem.Val1, problem.Operation, problem.Val2);
+            Console.WriteLine($"Answer is: {result}");
+            return 0;
+        }
+    }
+}
