1bd54c67849161e09360fa9f299c05a6bcad4414
LandyshH <86878681+LandyshH@users.noreply.github.com>
2021-10-17 17:43:36 +0300

    refactoring

--- Homework5/Calculator.fs
+++ Homework5/Calculator.fs
@@ -1,5 +1,5 @@
 module Homework5.Calculator
-let inline Calculate (val1: decimal, operation: CalculatorOperation, val2: decimal) =
+let inline Calculate (val1, operation: CalculatorOperation, val2) =
     match operation with
     | CalculatorOperation.Plus -> val1 + val2
     | CalculatorOperation.Multiply -> val1 * val2
