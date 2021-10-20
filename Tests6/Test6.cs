ac695681e98e00743cc1c219cddc1367ce8c50a2
LandyshH <86878681+LandyshH@users.noreply.github.com>
2021-10-20 16:07:56 +0300

    hw6

--- Tests6/Test6.cs
+++ Tests6/Test6.cs
@@ -1,7 +1,7 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
-using Homework6;
+
 
 namespace Tests6
 {
@@ -14,7 +14,8 @@ namespace Tests6
 		[InlineData("56.64", "7.8", "Multiply", "441.792")]
 		public async Task Program_CalculateProblem_ResultReturned(string val1, string val2, string operation, string expected)
 		{
-			var response = await new HttpClient().GetAsync($"http://localhost:5000/calculate?v1={val1}&Operation={operation}&v2={val2}");
+			var httpClient = new HttpClient();
+			var response = await httpClient.GetAsync($"http://localhost:5000/calculate?v1={val1}&Operation={operation}&v2={val2}");
 			var actual = await response.Content.ReadAsStringAsync();
 			Assert.Equal(expected, actual);
 		}
@@ -25,7 +26,8 @@ namespace Tests6
 		[InlineData("56.64", "7.8", "Not operation",  "\"Wrong operation.\"")]
 		public async Task Program_WrongCalculateProblem_ResultReturned(string val1, string val2, string operation, string expected)
 		{
-			var response = await new HttpClient().GetAsync($"http://localhost:5000/calculate?v1={val1}&Operation={operation}&v2={val2}");
+			var httpClient = new HttpClient();
+			var response = await httpClient.GetAsync($"http://localhost:5000/calculate?v1={val1}&Operation={operation}&v2={val2}");
 			var actual = await response.Content.ReadAsStringAsync();
 			Assert.Equal(expected, actual);
 		}
