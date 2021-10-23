23fe10315ab906cc952f60255bde3f562bf69911
LandyshH <86878681+LandyshH@users.noreply.github.com>
2021-10-23 19:34:24 +0300

    tests refactoting

---
+++ Tests2/ProgramTests.cs
@@ -0,0 +1,25 @@
+using Homework2;
+
+using NUnit.Framework;
+
+namespace CalculatorTests
+{
+    public class ProgramTests
+    {
+        [Test]
+        public void Program_AllArgumentsCorrect_0Returned()
+        {
+            var args = new[]{"9", "-", "1"};
+            var result = Homework2.Program.Main(args);
+            Assert.AreEqual(0, result);
+        }
+        
+        [Test]
+        public void Program_WrongOperation_1Returned()
+        {
+            var args = new[]{"9", "^", "2"};
+            var result = Program.Main(args);
+            Assert.AreEqual(1, result);
+        }
+    }
+}
