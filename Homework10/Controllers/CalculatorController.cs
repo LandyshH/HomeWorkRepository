286734b024c0cb2476475ca7bd0ffd52af59c76f
LandyshH <86878681+LandyshH@users.noreply.github.com>
2021-12-22 22:54:22 +0300

    i

--- Homework10/Controllers/CalculatorController.cs
+++ Homework10/Controllers/CalculatorController.cs
@@ -1,13 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
-<<<<<<< HEAD
-using Homework10.Calculator;
-=======
 using System.Threading.Tasks;
-using Homework10.CalculatorDependency;
+using Homework10.Calculator;
 using Homework10.ParallelCalculator;
->>>>>>> f08eca8 (hw10)
 using Homework10.StringExtensions;
 using Microsoft.AspNetCore.Mvc;
 
---
+++ Homework10/ParallelCalculator/IParallelCalculator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Homework10.ParallelCalculator
+{
+    public interface IParallelCalculator
+    {
+        Task<double> CalculateAsync(Dictionary<Expression, Expression[]> dependencies);
+    }
+}
