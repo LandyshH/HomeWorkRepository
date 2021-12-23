45abe22a11746aa3d0d75c585b105db5d7800eff
LandyshH <86878681+LandyshH@users.noreply.github.com>
2021-12-23 19:36:06 +0300

    refactoring

---
+++ Homework10/ParallelCalculator/IParallelCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Homework10.ParallelCalculator
+{
+    public interface IParallelCalculator
+    {
+        Task<double> CalculateAsync(Dictionary<Expression, Expression[]> dependencies);
+
+        Task<double> CalculateAsync(Expression current,
+            IReadOnlyDictionary<Expression, Expression[]> dependencies);
+    }
+}
