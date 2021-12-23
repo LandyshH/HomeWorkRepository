425457bda2cd9c17416d00d9aa43a67cd42523a0
LandyshH <86878681+LandyshH@users.noreply.github.com>
2021-12-23 17:21:08 +0300

    2k 511 (#11)
    * hw7
    
    * hw8
    
    * hw8
    
    * hw9
    
    * tests
    
    * Update dotnet.yml
    
    * tests
    
    * Update dotnet.yml
    
    * badge
    
    * .
    
    * Update dotnet.yml
    
    * refactoring
    
    * Update dotnet.yml
    
    * Update dotnet.yml
    
    * refactoring
    
    * Update dotnet.yml
    
    * Migrations
    
    * hw10
    
    * i
    
    * hw10
    
    * hw10
    
    * refactoring hw10

---
+++ Homework10/Calculator/ParallelCalculatorCache.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Homework10.Controllers;
+using Homework10.Database;
+using Homework10.Database.Models;
+using Homework10.ParallelCalculator;
+using Microsoft.EntityFrameworkCore;
+
+namespace Homework10.Calculator
+{
+    public class ParallelCalculatorCache : IParallelCalculator
+    {
+        public ParallelCalculatorCache(ApplicationContext applicationContext, 
+            ParallelCalculator.ParallelCalculator calculator)
+        {
+            ApplicationContext = applicationContext;
+            Calculator = calculator;
+        }
+
+        private ApplicationContext ApplicationContext { get; }
+        private IParallelCalculator Calculator { get; }
+        
+        public Task<double> CalculateAsync(Dictionary<Expression, Expression[]> dependencies)
+        {
+            return CalculateAsync(dependencies.First().Key, dependencies);
+        }
+
+        public async Task<double> CalculateAsync(Expression current,
+            IReadOnlyDictionary<Expression, Expression[]> dependencies)
+        {
+            var dbcalculation = ApplicationContext.Calculations
+                .FirstOrDefault(x => x.Expression == current.ToString());
+            var isExist = dbcalculation  is not null ;
+
+            if (!isExist)
+            {
+                var result = await Calculator.CalculateAsync(current, dependencies);
+                var calculation = new Calculation
+                {
+                    Expression = current.ToString(),
+                    Result = result.ToString(CultureInfo.InvariantCulture)
+                };
+                ApplicationContext.Calculations.Add(calculation);
+                await ApplicationContext.SaveChangesAsync();
+                return result;
+            }
+
+            var res = double.Parse(dbcalculation.Result);
+            return res;
+        }
+    }
+}
